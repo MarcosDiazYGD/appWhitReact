08a2e1c2fc2cac86023e125b0a978efa0039a81d
Marcos Diaz <progmarcosdiaz2022@gmail.com>
2022-10-24 19:12:28 -0500

    nuevos archivos

--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import phrases from '../quotes.json'
 
 const QuoteBox = () => {
-    const contPhrase = document.querySelector('.phrase')
-    const contauthor = document.querySelector('.author')
-    const heartIcon = document.querySelector('.fa-heart')
+    const initialIndex = Math.floor(Math.random() * phrases.length)
+    const [ index, setIndex ] = useState(initialIndex)
 
     let colorArray = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
     '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
@@ -21,27 +20,14 @@ const QuoteBox = () => {
     document.body.style = `background: ${colorArray[randomColorIndex]}`
     
 
-    const index = Math.floor(Math.random() * phrases.length)
-
     const clickRandom = ()=> {
-        const indexColor = Math.floor(Math.random() * colorArray.length)
-        document.body.style = `background: ${colorArray[indexColor]}`
-        const index = Math.floor(Math.random() * phrases.length)
-        const contPhrase = document.querySelector('.phrase')
-        const contauthor = document.querySelector('.author')
-        const heartIcon = document.querySelector('.fa-heart')
-
-        contPhrase.innerHTML = phrases[index].quote
-        contauthor.innerHTML = phrases[index].author
-
-        contPhrase.style= `color: ${colorArray[indexColor]}`
-        contauthor.style= `color: ${colorArray[indexColor]}`
-        heartIcon.style= `color: ${colorArray[indexColor]}`
+        const randomIndex = Math.floor(Math.random() * phrases.length)
+        setIndex(randomIndex);
     }
 
     return (
         <div>
-            <div className='QuoteBox'>
+            <div className='QuoteBox' style={{color:colorArray[randomColorIndex]}}>
                 <div className='phrase'>
                     {phrases[index].quote}
                 </div>
